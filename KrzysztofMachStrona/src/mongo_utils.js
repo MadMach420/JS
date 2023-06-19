0e92618ffaa59a0df8b2914eee8f435ae7347415
MadMach420 <mach.krzsztf@gmail.com>
2023-06-19 10:19:21 +0200

    js

---
+++ KrzysztofMachStrona/src/mongo_utils.js
@@ -0,0 +1,74 @@
+import { MongoClient } from 'mongodb'
+
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+await client.connect();
+
+
+export async function addProduct(newProduct) {
+    const collection = client.db('shop').collection('products');
+    collection.insertOne(newProduct);
+    return 200;
+}
+
+
+export async function updateProduct(name, quantity) {
+    const collection = client.db('shop').collection('products');
+    const products = await collection.find({ 'name': name }).toArray();
+
+    if (products.length != 1) {
+        return 400;
+    }
+
+    const product = products[0];
+    product.quantity = Math.max(0, parseInt(product.quantity) + parseInt(quantity));
+    
+    collection.replaceOne({ _id: product._id }, product);
+    return 200;
+}
+
+
+export async function sellProduct(personName, productName) {
+    const productCollection = client.db('shop').collection('products');
+    const products = await productCollection.find({ name: productName }).toArray();
+    if (products.length != 1) {
+        return 400;
+    }
+    const product = products[0];
+    product.quantity = Math.max(0, product.quantity - 1);
+    productCollection.replaceOne({ _id: product._id }, product);
+
+    delete product._id;
+    
+    const peopleCollection = client.db('shop').collection('people');
+    const people = await peopleCollection.find({ name: personName }).toArray();
+    if (people.length != 1) {
+        return 400;
+    }
+    const person = people[0];
+
+    person.products.push(product);
+    peopleCollection.replaceOne({ _id: person._id }, person);
+    return 200;
+}
+
+
+export async function getProducts() {
+    const collection = client.db('shop').collection('products');
+    const products = await collection.find({}).toArray();
+    const types = {};
+    products.forEach(elem => {
+        if (!(elem.type in Object.keys(types))) {
+            types[elem.type] = [];
+        }
+        types[elem.type].push(elem.subtype);
+    });
+
+    return { data: products, types: types };
+}
+
+
+export async function getPeople() {
+    const collection = client.db('shop').collection('people');
+    const people = await collection.find({}).toArray();
+    return people
+}
---
+++ KrzysztofMachStrona/src/public/buyer_script.js
@@ -0,0 +1,79 @@
+const cart = document.querySelector("#cart");
+const nameInput = document.querySelector("#name");
+let currentFilter = null;
+
+
+window.addEventListener("devicesContainerLoaded", updateDevices);
+
+
+function setTypes(types) {
+    const ul = document.querySelector("#navbar-nav-ul");
+    while (ul.firstChild) ul.removeChild(ul.lastChild);
+
+    for (const [type, subtypes] of Object.entries(types)) {
+        const li = document.createElement("li");
+        li.classList.add("nav-item", "dropdown");
+
+        li.innerHTML = `<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        ${type}
+        </a>`
+
+        const dropdown = document.createElement("ul");
+        dropdown.classList.add("dropdown-menu");
+
+        subtypes.forEach(subtype => {
+            const dropdownLi = document.createElement("li");
+            dropdownLi.classList.add("dropdown-item");
+            const text = document.createTextNode(subtype);
+            dropdownLi.appendChild(text);
+
+            dropdownLi.addEventListener("click", () => {
+                const newFilter = { type: type, subtype: subtype };
+                setNewFilter(newFilter);
+            });
+
+            dropdown.appendChild(dropdownLi);
+        });
+
+        li.appendChild(dropdown);
+        ul.appendChild(li);
+    }
+}
+
+
+async function updateDevices() {
+    const products = await getProducts();
+
+    setTypes(products.types);
+    window.dispatchEvent(new CustomEvent("updateDevices", { detail: products.data }));
+}
+
+
+function setNewFilter(newFilter) {
+    if (currentFilter && currentFilter.type == newFilter.type && currentFilter.subtype == newFilter.subtype) {
+        currentFilter = null;
+    } else {
+        currentFilter = newFilter;
+    }
+
+    window.dispatchEvent(new CustomEvent("setFilter", { detail: currentFilter }));
+}
+
+
+cart.addEventListener("dragover", (ev) => {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "copy";
+});
+
+
+cart.addEventListener("drop", (ev) => {
+    ev.preventDefault();
+    
+    const product = JSON.parse(ev.dataTransfer.getData("application/json"));
+    
+    const personName = nameInput.value;
+    if (personName){
+        sell(personName, product.name);
+    }
+});
